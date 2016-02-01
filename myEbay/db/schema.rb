a4661b57c18c8ceac8ec12d9ff2b82ca9acb169d
aurelien charreire <aureliencharreire@MacBook-Pro-de-aurelien.local>
2016-02-01 19:06:41 +0100

    ajout de product sans polymorphisme

--- myEbay/db/schema.rb
+++ myEbay/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160201145826) do
+ActiveRecord::Schema.define(version: 20160201174321) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.float    "price",       limit: 24
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
